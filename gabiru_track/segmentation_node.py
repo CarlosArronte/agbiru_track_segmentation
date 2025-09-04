b6db056d04d8469328663b9afbf084a2db55835c
CarlosArronte <carronte94@gmail.com>
2025-09-04 15:22:41 -0300

    Refactor SegmentationNode to integrate service and client for path optimization

--- gabiru_track/segmentation_node.py
+++ gabiru_track/segmentation_node.py
@@ -4,6 +4,7 @@ from std_msgs.msg import String,Bool
 from geometry_msgs.msg import PoseArray
 import numpy as np
 import json
+from std_srvs.srv import Trigger
 
 from ament_index_python.packages import get_package_share_directory
 from gabiru_track.segmentation import segment_track_by_curvature
@@ -11,32 +12,29 @@ from gabiru_track.segmentation import segment_track_by_curvature
 class SegmentationNode(Node):
     def __init__(self):
         super().__init__('segmentation_node')
-        self.publisher_ = self.create_publisher(String, 'gabiru/segments', 1000)
-        self.subscription = self.create_subscription(
+
+        self.srv = self.create_service(Trigger,'ready_to_recive_path',self.ready_callback) #1             
+        self.client = self.create_client(Trigger,'ready_to_recive_optimization')#3
+        self.publisher_ = self.create_publisher(String, 'gabiru/segments', 1000) #4 
+        
+        self.published = False  # Para publicar solo una vez
+        
+        self.waypoints = None
+        self.get_logger().info("Esperando waypoints en el tópico /optimal_path...")
+
+    def ready_callback(self,request, response):
+        self.subscription = self.create_subscription(#2
             PoseArray,
             '/optimal_path',
             self.path_callback,
             10)
-        self.can_recive_best_path_publisher= self.create_publisher(Bool, '/can_recive_best_path', 10)
-        self.can_recive_best_path = False
-        self.published = False  # Para publicar solo una vez
-        self.start_published = False
-        self.timer = self.create_timer(1.0, self.publish_start)
-        self.waypoints = None
-        self.get_logger().info("Esperando waypoints en el tópico /optimal_path...")
-
-    def publish_start(self):
-        if self.start_published:
-            return
-        self.start_published = True
-        msg = Bool()
-        msg.data = True
-        self.can_recive_best_path_publisher.publish(msg)
-        self.get_logger().info("Publicado can_recive_best_path=True")
-        self.can_recive_best_path = True
+        response.success = True
+        response.message = "PathOptimizer listo para recibir datos"
+        self.get_logger().info("Servicio ready_to_optimize: Suscripción a /processed_data activada")
+        return response   
 
     def path_callback(self, msg):
-        if self.published or not self.can_recive_best_path:            
+        if self.published:            
             return
 
         # Convertir los waypoints del mensaje PoseArray a un array de numpy
@@ -46,10 +44,26 @@ class SegmentationNode(Node):
             y = pose.position.y
             waypoints.append([x, y])
         self.waypoints = np.array(waypoints)
+        self.wait_for_ready()
+        
 
-        # Procesar y publicar los segmentosージャ
+    def wait_for_ready(self):
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Waiting for the Optimizer Node start")
+        request = Trigger.Request()
+        future = self.client.call_async(request)
+        future.add_done_callback(self.ready_response_callback)
 
-        self.load_segment_and_publish()
+    def ready_response_callback(self,future):
+        try:
+            response = future.result()
+            if response.success:
+                self.get_logger().info(f"PathExecutor listo: {response.message}")
+                self.load_segment_and_publish()
+            else:
+                self.get_logger().error(f"Fallo en la preparación: {response.message}")
+        except Exception as e:
+            self.get_logger().error(f"Error al llamar al servicio: {e}")
 
     def load_segment_and_publish(self):
         if self.published or self.waypoints is None:
